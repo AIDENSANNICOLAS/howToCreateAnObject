34ae1f7aaeb65ff6dfec16a4ab63c0b7d387bcd7
AIDENSANNICOLAS <sannicoa@wlhs.wlwv.k12.or.us>
2022-10-10 09:23:22 -0700

    Update Rectangle.java

--- BWERR/src/Rectangle.java
+++ BWERR/src/Rectangle.java
@@ -1,7 +1,14 @@
-public class Rectangle {
+public class Rectangle
+{
     private int width;
     private int height;
 
+
+    /**
+     * This is the constructor to create a Rectangle.
+     * To create a Rectangle we need to know its width
+     * and height.
+     */
 
     //
     public Rectangle(int rectWidth, int rectHeight)
@@ -9,8 +16,27 @@ public class Rectangle {
         width = rectWidth;
         height = rectHeight;
     }
+
+    //
+
+    //
+    public void printArea()  /**Note it will print the area of the Rectangle object that called it using the dot operator!*/
+    {
+        int area = width * height;
+        System.out.println(area);
+    }
+
     //
 
 
+
+
+    //
+    public String toString()   /** A toString method returns a String representation of the object.*/
+    {
+        return "Rectangle with width: " + width + " and height: " + height;
+    }
+
+    //
 
 }
