1dea295e1df07a986061de9f983b5754e7ca8d2c
AIDENSANNICOLAS <sannicoa@wlhs.wlwv.k12.or.us>
2022-10-10 09:03:32 -0700

    AddedFiles

---
+++ BWERR/src/Rectangle.java
@@ -0,0 +1,16 @@
+public class Rectangle {
+    private int width;
+    private int height;
+
+
+    //
+    public Rectangle(int rectWidth, int rectHeight)
+    {
+        width = rectWidth;
+        height = rectHeight;
+    }
+    //
+
+
+
+}
